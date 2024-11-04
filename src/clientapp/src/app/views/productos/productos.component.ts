3c4d983c543027c58e1e85117ffe2c5a2ac81559
Nicolas Fabian Caceres Cely <nfcaceres@muverang.com>
2024-11-04 14:50:09 -0500

    se garegan recurso

--- src/clientapp/src/app/views/productos/productos.component.ts
+++ src/clientapp/src/app/views/productos/productos.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { TableModule, UtilitiesModule,PageItemDirective, PageLinkDirective, PaginationComponent } from '@coreui/angular';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [],
+  imports: [
+    TableModule,
+    UtilitiesModule,
+    PageItemDirective, PageLinkDirective, PaginationComponent,
+    RouterLink
+  ],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.scss'
 })
